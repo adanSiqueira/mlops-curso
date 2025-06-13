50c7029c75734739e7a642315d822abac22ed5a8
Adan Siqueira <adan_siqueira@live.com>
2025-06-13 02:29:49 -0300

    atualizando main

--- project/main.py
+++ project/main.py
@@ -50,4 +50,4 @@ def cotacao():
     return jsonify({'preco_estimado': f'O Preço estimado do imóvel é {preco_formatado} $'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
